8e83562fa4b5baa490c2f71f681b23b419a8755c
Phan Manh Cuong <cuongpm.b23cn115@stu.ptit.edu.vn>
2025-05-30 12:16:07 +0700

    Add files via upload

---
+++ TRR1013.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<fstream>
+#include<set>
+
+using namespace std;
+
+void fileIO()
+{
+	freopen("DT.INP","r",stdin);
+	freopen("DT.OUT","w",stdout);
+}
+
+class Graph{
+	private:
+		int n;
+		vector<vector<int>> matranke;
+		vector<vector<int>> danhsachke;
+	public:
+	Graph(int n):n(n){
+		danhsachke.resize(n+1);
+		matranke=vector<vector<int>> (n+1,vector<int>(n+1));
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=n;j++)
+			{
+				cin>>matranke[i][j];
+				if(matranke[i][j]==1) addEdge(i,j);
+			}
+		}
+	}
+	void addEdge(int u,int v){
+		danhsachke[u].push_back(v);
+	}
+	void display1(){
+		for(int i=1;i<=n;i++)
+		{	
+			int indeg=0;int outdeg=0;
+			for(int j=1;j<=n;j++)
+			{
+				indeg+=matranke[j][i];
+				outdeg+=matranke[i][j];
+			}
+			cout<<indeg<<" "<<outdeg<<"\n";
+		}
+	}
+	void display2(){
+		cout<<n<<"\n";
+		for(int i=1;i<=n;i++)
+		{
+			cout<<danhsachke[i].size()<<" ";
+			for(int v:danhsachke[i])
+			{
+				cout<<v<<" ";
+			}
+			cout<<"\n";
+		}
+	}
+};
+
+void solve(int t)
+{
+	int n;
+	cin>>n;
+	Graph g(n);
+	if(t==1) g.display1();
+	if(t==2) g.display2();
+}
+
+int main()
+{
+	fileIO();
+	int t;cin>>t;solve(t);
+	return 0;
+}
